9b47c176dce66a026c91a14a5bf62e4d170fcea6
Felipe Kinupes Gouvea <felipekinupesg@hotmail.com>
2021-04-29 14:05:13 -0300

    Projeto Criado

---
+++ src/main/java/br/com/cerg/siscerg/SiscergApplication.java
@@ -0,0 +1,13 @@
+package br.com.cerg.siscerg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SiscergApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SiscergApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/cerg/siscerg/SiscergApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.cerg.siscerg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SiscergApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
