1a80c70fc17e53b4085a98c3b0a07af44bc4b505
Felipe Kinupes Gouvea <felipekinupesg@hotmail.com>
2021-05-01 11:28:44 -0300

    Atualizacao: Utilizando somento JsonIgnore

--- src/main/java/br/com/cerg/siscerg/entities/Endereco.java
+++ src/main/java/br/com/cerg/siscerg/entities/Endereco.java
@@ -9,6 +9,7 @@ import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 
 import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -31,7 +32,7 @@ public class Endereco implements Serializable{
 	private String bairro;
 	private String cep;
 	
-	@JsonBackReference
+	@JsonIgnore
 	@ManyToOne
 	@JoinColumn(name = "cliente_id")
 	private Cliente cliente;
